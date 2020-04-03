d77200cc3030aa8358adb0af2ff2d9482a5049b4
Rosario <rosario.russo@blastingnews.com>
2020-04-03 22:07:10 +0200

    Global update for first release

--- src/assets/javascripts/menu.js
+++ src/assets/javascripts/menu.js
@@ -1,3 +1,10 @@
 const openerMenu = document.querySelector('.c-nav__toggle-input');
+const menu = document.querySelector('.c-nav__menu');
 
-console.log(openerMenu);
+openerMenu.addEventListener('click', function(){
+  if(this.checked === true) {
+    menu.classList.add('c-nav__menu--open');
+  } else {
+    menu.classList.remove('c-nav__menu--open');
+  }
+});
